0a253d1688ac4375b1d9e317a406e28043faff37
Harsh Sharma <harsh.sharma@loylty.com>
2026-03-01 01:22:20 +0530

    updated nginx config file

--- vite.config.js
+++ vite.config.js
@@ -14,8 +14,8 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        // target: 'http://k8s-ingressn-ingressn-09f0faf155-fc4a3bcb6620c60e.elb.ap-south-1.amazonaws.com/dev/egv-backend',
-        target: 'http://localhost:9100',
+        target: 'http://k8s-ingressn-ingressn-09f0faf155-fc4a3bcb6620c60e.elb.ap-south-1.amazonaws.com/dev/egv-backend',
+        // target: 'http://localhost:9100',
         changeOrigin: true
       }
     }
